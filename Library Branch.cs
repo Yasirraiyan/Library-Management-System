dbc85d0cd94e2192eda231c5065e13baea22a511
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-14 14:29:32 +0600

    Create Library Branch.cs

---
+++ Library Branch.cs
@@ -0,0 +1,7 @@
+public class LibraryBranch
+{
+    public int BranchID { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string ContactInfo { get; set; }
+}
