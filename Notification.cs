9f9cd93beb889b35b3bafd7f17f1afed2b2cf8d8
Yasirraiyan <127099527+Yasirraiyan@users.noreply.github.com>
2025-01-14 14:29:14 +0600

    Create Notification.cs

---
+++ Notification.cs
@@ -0,0 +1,7 @@
+public class Notification
+{
+    public int NotificationID { get; set; }
+    public Member Member { get; set; }
+    public string Message { get; set; }
+    public DateTime Date { get; set; }
+}
